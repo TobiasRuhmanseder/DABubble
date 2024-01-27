c62f5405c94bc1d8a4da47213521fcb092d55231
Tobias Ruhmanseder <tobi.r1707@gmail.com>
2024-01-27 11:20:58 +0100

    start with html and css on the main page - add the first components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,14 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { HeaderMenuComponent } from './header-menu/header-menu.component';
+import { SideMenuComponent } from './side-menu/side-menu.component';
+import { MainContentComponent } from './main-content/main-content.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, HeaderMenuComponent, SideMenuComponent, MainContentComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
