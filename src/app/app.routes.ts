129840eeec91ee7810a34a19c9ba80f1ed0cc7bf
Björn Daigger <124874016+bjoerndaigger@users.noreply.github.com>
2024-01-30 13:16:46 +0100

    css sign-up component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,14 @@
 import { Routes } from '@angular/router';
 import { HomeScreenComponent } from './home-screen/home-screen.component';
 import { LoginComponent } from './start-screen/login/login.component';
+import { SignInComponent } from './start-screen/sign-in/sign-in.component';
+import { LegalNoticeComponent } from './legal-notice/legal-notice.component';
+import { PrivacyPolicyComponent } from './privacy-policy/privacy-policy.component';
 
 export const routes: Routes = [
     { path: '', component: LoginComponent },
+    { path: 'sign-in', component: SignInComponent },
     { path: 'home', component: HomeScreenComponent },
+    { path: 'legal-notice', component: LegalNoticeComponent },
+    { path: 'privacy-policy', component: PrivacyPolicyComponent }
 ];
--- src/app/start-screen/sign-in/sign-in.component.ts
+++ src/app/start-screen/sign-in/sign-in.component.ts
@@ -1,12 +1,31 @@
 import { Component } from '@angular/core';
+import { FormGroup, FormControl, ReactiveFormsModule } from '@angular/forms';
+import { Router, RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-sign-in',
   standalone: true,
-  imports: [],
+  imports: [
+    ReactiveFormsModule, 
+    RouterLink
+  ],
   templateUrl: './sign-in.component.html',
   styleUrl: './sign-in.component.scss'
 })
-export class SignInComponent {
 
+export class SignInComponent {
+constructor(private router: Router) {}
+  profileForm = new FormGroup({
+    name: new FormControl(''),
+    email: new FormControl(''),
+    password: new FormControl(''),
+  });
+
+  navigateToLegalNotice() {
+    this.router.navigate(['/legal-notice']);
+  }
+
+  navigateToPrivacyPolicy() {
+    this.router.navigate(['/privacy-policy']);
+  }
 }
