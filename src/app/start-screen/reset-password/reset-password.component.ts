8d1751f72148629a8c4fdc19d28afa1d12d3c24f
Björn Daigger <124874016+bjoerndaigger@users.noreply.github.com>
2024-02-04 21:05:25 +0100

    css reset password component

--- src/app/start-screen/reset-password/reset-password.component.ts
+++ src/app/start-screen/reset-password/reset-password.component.ts
@@ -1,12 +1,26 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-reset-password',
   standalone: true,
-  imports: [],
+  imports: [
+  ],
   templateUrl: './reset-password.component.html',
   styleUrl: './reset-password.component.scss'
 })
 export class ResetPasswordComponent {
+  constructor(private router: Router) { }
 
+  navigateToLogin() {
+    this.router.navigate(['']);
+  }
+
+  navigateToLegalNotice() {
+    this.router.navigate(['/legal-notice']);
+  }
+
+  navigateToPrivacyPolicy() {
+    this.router.navigate(['/privacy-policy']);
+  }
 }
