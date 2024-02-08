9d2ad158881099779e18669cbf6a15a0a43c4f21
Riedmann Bastian <Riedmann.Basti@gmail.com>
2024-02-08 20:46:02 +0100

    popup channel add user

--- src/app/main-content/main-contents/main-content-header/main-content-header.component.ts
+++ src/app/main-content/main-contents/main-content-header/main-content-header.component.ts
@@ -1,11 +1,16 @@
 import { Component } from '@angular/core';
 import { MessageService } from '../../../../services/message.service';
 import { CommonModule } from '@angular/common';
+import { DialogUserInfoComponent } from '../../private-chat/dialog-user-info/dialog-user-info.component';
+import { MatDialog } from '@angular/material/dialog';
+import { ChannelPopUpComponent } from '../../channel/channel-pop-up/channel-pop-up.component';
+import { FormsModule } from '@angular/forms';
+import { MatMenuModule } from '@angular/material/menu';
 
 @Component({
   selector: 'app-main-content-header',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, FormsModule, MatMenuModule],
   templateUrl: './main-content-header.component.html',
   styleUrl: './main-content-header.component.scss',
 })
@@ -13,17 +18,48 @@ export class MainContentHeaderComponent {
   showBg: boolean = false;
   addUserDialog: boolean = false;
   userDialog: boolean = false;
-  constructor(public chatService: MessageService) {}
+  disableButton: boolean = true;
+  inputAddUser: string = '';
+  constructor(
+    public chatService: MessageService,
+    public userDetails: MatDialog,
+    public channelDetails: MatDialog
+  ) {}
 
+  selectUser(user: string) {
+    this.inputAddUser = user;
+    this.disableButton = false;
+  }
+
+  closeUser() {
+    this.inputAddUser = '';
+    this.disableButton = true;
+  }
+
+  // muss noch im firebase gespeichert werden!
+  addUserToChannel() {
+    this.chatService.currentChannel[0].users.push(this.inputAddUser);
+  }
+
+  openUserDetails() {
+    this.userDetails.open(DialogUserInfoComponent, {});
+  }
 
   closeAllDialog() {
     this.showBg = false;
     this.addUserDialog = false;
     this.userDialog = false;
+    this.inputAddUser = '';
+    this.disableButton = true;
   }
 
   addUser() {
     this.showBg = !this.showBg;
+    this.addUserDialog = true;
+  }
+
+  goToAddUser() {
+    this.userDialog = false;
     this.addUserDialog = true;
   }
 
@@ -33,7 +69,7 @@ export class MainContentHeaderComponent {
   }
 
   openChannelData() {
-    this.showBg = !this.showBg;
+    this.channelDetails.open(ChannelPopUpComponent, {});
   }
 
   getChannelUsersLength() {
