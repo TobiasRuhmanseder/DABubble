4b9f986b1cfb1f6ba01dc7f37eb832ded9aee206
Riedmann Bastian <Riedmann.Basti@gmail.com>
2024-02-14 19:10:41 +0100

    add emojis

--- src/app/main-content/main-contents/main-content-footer/main-content-footer.component.ts
+++ src/app/main-content/main-contents/main-content-footer/main-content-footer.component.ts
@@ -4,13 +4,19 @@ import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { MainContentComponent } from '../../main-content.component';
 import { IconHoverChangeImageComponent } from '../../../icon-hover-change-image/icon-hover-change-image.component';
+import { PickerModule } from '@ctrl/ngx-emoji-mart';
 
 @Component({
   selector: 'app-main-content-footer',
   standalone: true,
   templateUrl: './main-content-footer.component.html',
   styleUrl: './main-content-footer.component.scss',
-  imports: [CommonModule, FormsModule, IconHoverChangeImageComponent],
+  imports: [
+    CommonModule,
+    FormsModule,
+    IconHoverChangeImageComponent,
+    PickerModule,
+  ],
 })
 export class MainContentFooterComponent {
   @Input() mainChat: any;
@@ -22,9 +28,20 @@ export class MainContentFooterComponent {
 
   textAreaContent: string = '';
   textAreaThreadContent: string = '';
+  isEmojiPickerVisible: boolean = false;
 
+  addEmoji(event: { emoji: { native: any } }, mainChat: any) {
+    if (mainChat) {
+      this.textAreaContent = `${this.textAreaContent}${event.emoji.native}`;
+    } else {
+      this.textAreaThreadContent = `${this.textAreaThreadContent}${event.emoji.native}`;
+    }
+    this.isEmojiPickerVisible = false;
+  }
   getPlaceholderText(): string {
-    return this.mainChat ? `Nachricht an # ${this.chatService.getChannelName()}` : 'Antworten...';
+    return this.mainChat
+      ? `Nachricht an # ${this.chatService.getChannelName()}`
+      : 'Antworten...';
   }
 
   sendMessage() {
@@ -32,7 +49,6 @@ export class MainContentFooterComponent {
       let message = this.chatService.setMessage(this.textAreaContent);
       this.textAreaContent = '';
       this.chatService.saveAndAddNewMessage(message);
-
     } else {
       let message = this.chatService.setMessage(this.textAreaThreadContent);
       this.textAreaThreadContent = '';
