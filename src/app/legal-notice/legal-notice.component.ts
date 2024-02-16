eefc1ce2eb12b4e2f5a6f25490d54bf3fbd7d173
Björn Daigger <124874016+bjoerndaigger@users.noreply.github.com>
2024-02-16 13:24:26 +0100

    legal notice & privacy policy

--- src/app/legal-notice/legal-notice.component.ts
+++ src/app/legal-notice/legal-notice.component.ts
@@ -1,12 +1,19 @@
 import { Component } from '@angular/core';
+import { Router, RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-legal-notice',
   standalone: true,
-  imports: [],
+  imports: [
+    RouterLink
+  ],
   templateUrl: './legal-notice.component.html',
   styleUrl: './legal-notice.component.scss'
 })
 export class LegalNoticeComponent {
-
+  constructor(private router: Router) { }
+  
+  navigateToLogin() {
+    this.router.navigate(['']);
+  }
 }
