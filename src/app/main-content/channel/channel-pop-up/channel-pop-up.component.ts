6188249e1f333c086698be2a638638d73132b4c6
Riedmann Bastian <Riedmann.Basti@gmail.com>
2024-03-26 18:50:25 +0100

    edit Channel

--- src/app/main-content/channel/channel-pop-up/channel-pop-up.component.ts
+++ src/app/main-content/channel/channel-pop-up/channel-pop-up.component.ts
@@ -4,16 +4,15 @@ import { FormsModule } from '@angular/forms';
 import { MatInputModule } from '@angular/material/input';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import {
-  MatDialog,
   MAT_DIALOG_DATA,
   MatDialogRef,
-  MatDialogTitle,
-  MatDialogContent,
-  MatDialogActions,
-  MatDialogClose,
   MatDialogModule,
 } from '@angular/material/dialog';
 import { UsersService } from '../../../../services/users.service';
+import { CommonModule } from '@angular/common';
+import { MessageService } from '../../../../services/message.service';
+import { FirebaseService } from '../../../../services/firebase.service';
+import { Channel } from '../../../../models/channel.class';
 
 @Component({
   selector: 'app-channel-pop-up',
@@ -24,20 +23,52 @@ import { UsersService } from '../../../../services/users.service';
     FormsModule,
     MatButtonModule,
     MatDialogModule,
-
+    CommonModule,
   ],
   templateUrl: './channel-pop-up.component.html',
-  styleUrl: './channel-pop-up.component.scss'
+  styleUrl: './channel-pop-up.component.scss',
 })
 export class ChannelPopUpComponent {
-channelData:any;
+  channelData: any;
+  channelNameInput: string = '';
+  channelDescriptionInput: string = '';
+  editName: boolean = false;
+  editDescription: boolean = false;
   constructor(
+    private fire: FirebaseService,
     public users: UsersService,
     public dialogRef: MatDialogRef<ChannelPopUpComponent>,
-    @Inject(MAT_DIALOG_DATA) public data:any
-  ) { 
-    this.channelData = this.data.channel
-    console.log(this.channelData)
+    @Inject(MAT_DIALOG_DATA) public data: any
+  ) {
+    this.channelData = this.data.channel;
+    this.channelNameInput = this.channelData.name;
+    this.channelDescriptionInput = this.channelData.description;
+    console.log(this.channelData);
+  }
+
+  editChannel(property: string) {
+    if (property === 'name') {
+      this.channelNameInput = this.channelData.name;
+      this.editName = true;
+    }
+    if (property === 'description') {
+      this.channelDescriptionInput = this.channelData.description;
+      this.editDescription = true;
+    }
+  }
+
+  saveEdit(property: string) {
+    if (property === 'name') {
+      this.channelData.name = this.channelNameInput;
+      this.editName = false;
+    }
+    if (property === 'description') {
+      this.channelData.description = this.channelDescriptionInput;
+      this.editDescription = false;
+    }
+
+    let newData = new Channel(this.channelData);
+    this.fire.updateChannel(newData);
   }
 
   onNoClick(): void {
