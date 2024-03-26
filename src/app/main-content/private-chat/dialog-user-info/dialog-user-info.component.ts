72b37d6e0b79aa6b07b72b1e02da37e8392204ac
Riedmann Bastian <Riedmann.Basti@gmail.com>
2024-03-26 20:20:33 +0100

    style edit channel

--- src/app/main-content/private-chat/dialog-user-info/dialog-user-info.component.ts
+++ src/app/main-content/private-chat/dialog-user-info/dialog-user-info.component.ts
@@ -1,33 +1,66 @@
-import { Component, Inject } from '@angular/core';
+import { Component, Inject, inject } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
 import { UsersService } from '../../../../services/users.service';
 import { CommonModule } from '@angular/common';
+import { DirectMessagesService } from '../../../../services/direct-messages.service';
+import { Router } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+import { MessageService } from '../../../../services/message.service';
 
 @Component({
   selector: 'app-dialog-user-info',
   standalone: true,
-  imports: [CommonModule, MatButtonModule],
+  imports: [CommonModule, MatButtonModule, FormsModule],
   templateUrl: './dialog-user-info.component.html',
   styleUrl: './dialog-user-info.component.scss',
 })
 export class DialogUserInfoComponent {
   userData: any;
-
+  edit: boolean = false;
+  editable: boolean = false;
+  diMeService: DirectMessagesService = inject(DirectMessagesService);
+  router: Router = inject(Router);
+  userNameInput: string = '';
+  userEmailInput: string = '';
   constructor(
     public users: UsersService,
     public dialogRef: MatDialogRef<DialogUserInfoComponent>,
+    private chatService: MessageService,
     @Inject(MAT_DIALOG_DATA) public data: any
   ) {
     this.userData = this.users.getUserFromId(data.id);
+    if (this.userData.id === this.chatService.currentUser) {
+      this.editable = true;
+    } else {
+      this.editable = false;
+    }
+  }
+
+  editUser() {
+    this.edit = true;
+    this.userNameInput = this.userData.name;
+    this.userEmailInput = this.userData.email;
   }
 
   sendEmail(email: string) {
     window.open('mailto:' + email);
   }
+  abort() {
+    this.edit = false;
+  }
+  save() {
+    
+    this.abort();
 
-  goToPrivateChannel(): void {
-    this.onNoClick();
+  }
+  async goToPrivateChannel(userId: string) {
+    if (userId != undefined) {
+      let directMessageDocId =
+        await this.diMeService.getDocIdFromTheDirectMessaging(userId);
+      this.router.navigateByUrl('/home/' + directMessageDocId);
+      this.onNoClick();
+    }
   }
 
   onNoClick(): void {
